7959040c278b09525bc47e2dab6c9570e7952583
jasonbringle <jasonbringle@gmail.com>
2020-08-02 18:23:12 -0700

    Server configured and ready

---
+++ projects/project-model.js
@@ -0,0 +1,6 @@
+const db = require('../data/db-config.js');
+const { json } = require('express');
+
+module.exports= {
+    
+}
---
+++ projects/project-router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const Db = require('./project-model.js');
+const dbConfig = require('../data/db-config.js');
+
+const router = express.Router();
+
+
+module.exports = router;
