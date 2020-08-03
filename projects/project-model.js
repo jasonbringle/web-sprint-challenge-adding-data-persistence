2cdc335bdfe627f89828a8025f4155f5dd61fc8b
jasonbringle <jasonbringle@gmail.com>
2020-08-02 18:57:09 -0700

    built out router and model

--- projects/project-model.js
+++ projects/project-model.js
@@ -2,5 +2,53 @@ const db = require('../data/db-config.js');
 const { json } = require('express');
 
 module.exports= {
-    
+    getProjects,
+    addResource,
+    getresources,
+    addProject,
+    addTask,
+    getTasksFromProject
+}
+
+function addResource(resource){
+    return db("resource")
+        .insert(resource)
+        .then(added => {
+            console.log(added)
+            return db("resource").where({id:added})
+        })
+}
+
+function getresources(){
+    return db("resource")
+}
+
+function addProject(project){
+    const newProject = project
+    return db("projects")
+        .insert(newProject)
+        .then(project => {
+            return db("projects").where({id:project[0]})
+        })
+}
+
+function getProjects(){
+    return db("projects")
+}
+
+function addTask(task){
+    const newTask = task
+    return db("task")
+        .insert(newTask)
+        .then(addedTask => {
+            return db("task").where({id:addedTask[0]})
+        })
+}
+
+function getTasksFromProject(id){
+    console.log(id)
+    return db('task')
+        .join("projects", "task.id", "=", "projects.id")
+        .select("projects.name", "projects.description", "task.description", "task.notes")
+        .where({"projects.id": id})
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -5,5 +5,78 @@ const dbConfig = require('../data/db-config.js');
 
 const router = express.Router();
 
+router.get('/projects', (req, res) => {
+    Db.getProjects()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/resource', (req,res) => {
+    const body = req.body
+    Db.addResource(body)
+        .then(resource =>{
+            if(resource){
+                res.status(200).json(resource)
+            }{res.status(500).json({error: 'Could not return the resource'})}
+        })
+        .catch(error => {
+            res.status.apply(500).json({ errormessage: "Could not add resource"})
+        })
+})
+
+router.get("/resources", (req,res) => {
+    Db.getresources()
+        .then(list => {
+            res.status(200).json(list)
+        })
+        .catch(err => {
+            res.status(500).json({errormessage: "Could not retrieve list of resources"})
+        })
+})
+
+router.post("/project", (req,res) => {
+    const body = req.body
+    Db.addProject(body)
+        .then(added => {
+            console.log("Added name of project", added)
+            if(added){res.status(200).json(added[0])}{
+                res.status(500).json({errormessage:"Could not post project."}) 
+            }
+        })
+        .catch(error => {
+            res.status(500).json({errormessage:"Could not post project."})
+        })
+})
+
+router.post("/task", (req,res) => {
+    const body = req.body
+    Db.addTask(body)
+        .then(added => {
+            console.log("Added name of task", added)
+            if(added){res.status(200).json(added[0])}{
+                res.status(500).json({errormessage:"Could not post task."}) 
+            }
+        })
+        .catch(error => {
+            res.status(500).json({errormessage:"Could not post task."})
+        })
+})
+
+router.get("/:id/task", (req,res) => {
+    const { id } = req.params
+    Db.getTasksFromProject(id)
+        .then(tasks => {
+            res.status(200).json(tasks)
+        })
+        .catch(err =>{
+            res.status(500).json({ errormessage: "Could not retrieve"})
+        })
+})
+
+
 
 module.exports = router;
